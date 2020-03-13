3a42252478eddfb9d1e5bce9e8cec4175be2c27b
Oscar Barber-Wilson <oscarbarberwilson@gmail.com>
2020-03-13 13:08:43 +1300

    Added entire toon shader unity project
    Added the unity project folder to the repo

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Vector3 moveDirection;
+
+    private Camera mainCamera;
+
+    public bool useController;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Vector3 playerDirection = Vector3.right * -Input.GetAxisRaw("Horizontal") + Vector3.forward * -Input.GetAxisRaw("Vertical");
+        if(playerDirection.sqrMagnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# midnight-snack-jam3
- Week long prototype of Midnight Snack
